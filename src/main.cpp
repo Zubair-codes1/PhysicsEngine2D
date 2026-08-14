d42d169543c65abbe496e91234076245af0e21ba
Zubair <zubairabdulmatin@gmail.com>
2026-08-14 20:26:25 +0100

    UPDATE: working on mouse events

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML setup");
+    window.setFramerateLimit(60);
+
+    // mouse
+    sf::Mouse::Button mouseLeftButton(sf::Mouse::Button::Left);
     float radius = 100.f;
     sf::CircleShape shape(radius, 50.f);
     sf::Time pauseTime = sf::seconds(1);
     shape.setFillColor(sf::Color::Green);
 
     while(window.isOpen()) {
+
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
@@ -17,15 +22,13 @@ int main() {
 
         window.clear();
 
-        shape.setRadius(radius);
+        if (sf::Mouse::isButtonPressed(mouseLeftButton)) {
+            sf::Vector2i mousePosition = sf::Mouse::getPosition();
+            shape.setPosition(sf::Vector2f(mousePosition.x, mousePosition.y));
+            window.draw(shape);
+        }
 
         window.draw(shape);
-
-        //radius--;
-        sf::Vector2f pos = shape.getPosition();
-        pos += sf::Vector2{10.f, 10.f};
-        shape.setPosition(pos);
-        sf::sleep(pauseTime);
         window.display();
     }
 
