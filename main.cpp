fcc23046d6a4959e89b02136878e16a78e55594e
Zubair <zubairabdulmatin@gmail.com>
2026-08-14 19:08:36 +0100

    UPDATE: main.cpp playing around with movement and shapes in sfml

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML setup");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode({800, 600}), "SFML setup");
+    float radius = 100.f;
+    CircleShape shape(radius, 50.f);
+    Time pauseTime = seconds(1);
+    shape.setFillColor(Color::Green);
 
     while(window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -13,7 +19,16 @@ int main() {
         }
 
         window.clear();
+
+        shape.setRadius(radius);
+
         window.draw(shape);
+
+        //radius--;
+        Vector2f pos = shape.getPosition();
+        pos += Vector2{10.f, 10.f};
+        shape.setPosition(pos);
+        sleep(pauseTime);
         window.display();
     }
 
