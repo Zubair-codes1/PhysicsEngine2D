b4a33bc3fb3f545f08e3b0e2faac2c69e1484e73
Zubair <zubairabdulmatin@gmail.com>
2026-08-15 16:19:03 +0100

    Added: Rigidbody class

---
+++ src/RigidBody.hpp
@@ -0,0 +1,17 @@
+#ifndef RIGIDBODY
+
+#define RIGIDBODY
+
+namespace physics {
+    class RigidBody {
+        public:
+        
+
+        private:
+            float mass;
+            float velocity;
+            float acceleration;
+    };
+}
+
+#endif
